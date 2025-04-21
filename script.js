45accc154776b478c4186f1d31e078d8d62777be
ub-victor <victoireushindi371@gmail.com>
2025-04-21 23:05:53 +0200

    Initial Commit

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-// Function to scroll to specific section  
-function scrollToSection(sectionId) {  
+unction scrollToSection(sectionId) {  
     const section = document.getElementById(sectionId);  
     if (section) {  
         section.scrollIntoView({ behavior: 'smooth' });  
     }  
 }  
 
-// Adding click event listeners to each list item  
 document.getElementById('about').onclick = function() {  
     scrollToSection('about');  
-};  
-
-document.getElementById('journey').onclick = function() {  
-    scrollToSection('journey');  
-};  
-
-document.getElementById('goals').onclick = function() {  
-    scrollToSection('goals');  
-};  
+};
