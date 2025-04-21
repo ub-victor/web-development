24114ac8a01cab8de01e526364f61c1b8b20043f
ub-victor <victoireushindi371@gmail.com>
2025-04-21 22:56:30 +0200

    Initial Commit

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-    // Function to scroll to specific section  
-    function scrollToSection(sectionId) {  
-        const section = document.getElementById(sectionId);  
-        if (section) {  
-            section.scrollIntoView({ behavior: 'smooth' });  
-        }  
+// JavaScript for smooth scrolling  
+function scrollToSection(sectionId) {  
+    const section = document.getElementById(sectionId);  
+    if (section) {  
+        section.scrollIntoView({ behavior: 'smooth' });  
     }  
+}  
 
-    // Add click listeners to the list items  
-    document.getElementById('about').onclick = function() {  
-        scrollToSection('about');  
-    };  
+// Adding click event listeners to the list items  
+document.getElementById('about').onclick = function() {  
+    scrollToSection('about');  
+};  
 
-    document.getElementById('journey').onclick = function() {  
-        scrollToSection('journey');  
-    };  
+document.getElementById('journey').onclick = function() {  
+    scrollToSection('journey');  
+};  
 
-    document.getElementById('goals').onclick = function() {  
-        scrollToSection('goals');  
-    }; 
+document.getElementById('goals').onclick = function() {  
+    scrollToSection('goals');  
+};  
