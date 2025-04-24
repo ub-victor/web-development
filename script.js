f92f0e800fc3b1d4b4b8bbb54ae8e29d36cdf76a
ub-victor <victoireushindi371@gmail.com>
2025-04-24 19:18:47 +0200

    Initial Commit

--- script.js
+++ script.js
@@ -17,4 +17,4 @@ function changeBackgroundColor(color) {
 // Get first element of each collection and add event listeners
 document.querySelector(".intro").addEventListener('click', () => changeBackgroundColor("#2CA02C"));
 document.querySelector(".react").addEventListener('click', () => changeBackgroundColor("#FF7F0E"));
-document.querySelector(".backend").addEventListener('click', () => changeBackgroundColor("#9467BD")); 
+document.querySelector(".backend").addEventListener('click', () => changeBackgroundColor("#9467BD"));  
